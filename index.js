43a5ee06017dd2f3c1884fa9427c2639d2445258
Hridrita <moonsaha721@gmail.com>
2026-05-29 23:29:03 +0600

    created database and one collection

--- index.js
+++ index.js
@@ -26,10 +26,17 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const db = client.db("docure")
+    const doctorsCollection = db.collection("doctors")
+
+    
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
